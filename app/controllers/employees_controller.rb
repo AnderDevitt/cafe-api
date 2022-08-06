5c51897586c4d8ebc79021c398c07d1cbb099a3d
RaDevy <ranjithrajesh256@gmail.com>
2022-08-06 14:55:46 +1000

    try and impliment employee tests

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
+    before_action :set_employee, only: [:show, :update, :destroy]
 
     # GET /employees
     def index
@@ -70,9 +70,9 @@ class EmployeesController < ApplicationController
         # if !@role.nil? && @role.authenticate(params[:password])
         if !@employee.nil? && BCrypt::Password.new(@employee.password) == params[:password]
             pp("Employee found!")
-            auth_token = Knock::AuthToken.new(payload: {sub: @employee.id}).token 
-            pp("Knock made an auth token: " + auth_token)
-            render json: {username: @employee.username, jwt: auth_token}, status: 200
+            # auth_token = Knock::AuthToken.new(payload: {sub: @employee.id}).token 
+            # pp("Knock made an auth token: " + auth_token)
+            render json: {username: @employee.username}, status: 200
         else
             render json: {Error: "Invalid username or password"}
         end
