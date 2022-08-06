5a66ad7480ffd1021e3ccee9faf2d861f6625c18
Andrew Devitt <gcab022116@coderacademy.edu.au>
2022-08-06 20:28:16 +1000

    cors address change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 # Allow access to both the localhost:3000 port and the deployed Netlify site
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://staging-wonderful-meerkat-7d4755.netlify.app', 'http://localhost:3000'
+    origins 'https://wonderful-meerkat-7d4755.netlify.app'
 
     resource '*',
       headers: :any,
